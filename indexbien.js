a8c4a55830d18fe4cdbca25b39aebf0a0087f116
trandangkhoa <trandangkhoastt26@gmail.com>
2021-01-20 19:52:48 +0700

    js

---
+++ indexbien.js
@@ -0,0 +1,16 @@
+/*function tinhtong(){
+    let a = +prompt("nhap so nguyen a");
+    let b = +prompt("nhap so nguyen b");
+    c = a + b;
+    alert(c);
+   
+}
+tinhtong()*/
+function ngaythangnam() {
+    let d = prompt("nhap ngay");
+    let m = prompt("nhap thang");
+    let y = prompt("nhap nam");
+    alert(d + "-" + m +"-"+ y);
+}
+ngaythangnam();
+
